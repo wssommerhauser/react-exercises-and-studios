900bb17b0dd4b5214dff5c980acd639ff4ded208
William Sommerhauser <wssommerhauser@gmail.com>
2023-11-20 18:01:42 -0600

    part one complete

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1334416842i/830502.jpg";
+   let book2 = "https://media.tenor.com/XSns7DzGv38AAAAC/cat-running.gif";
+   let book3 = "https://149522020.v2.pressablecdn.com/wp-content/uploads/2020/06/b960fb_82311cf8f41b4194a0884c1173537658mv2.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="It" />
+         <img src={book2} alt="Apple Cat: The Book" />
+         <img src={book3} alt="JAWS" />
       </div>      
    );
 }
